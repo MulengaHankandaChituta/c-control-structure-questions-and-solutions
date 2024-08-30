00d8b0432b84268683e1d22dc50460609fd26b65
MulengaHankandaChituta <mulengahankandachituta@gmail.com>
2024-08-30 10:28:48 +0200

    exercise5 program that displays message indicating whether triangle is right triangle or not

---
+++ exercise5.cpp
@@ -0,0 +1,26 @@
+// In a right triangle, the square of the length
+//of one side is equal to the sum of the squares
+// of the other two sides. Write a program that
+//prompts the user to enter the lengths of the 
+// three sides of a triangle and the outputs
+// a message indicating whether the triangle 
+// is a right triangle
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int side1;
+    int side2;
+    int side3 = 61;
+
+    cout << "Enter side1 and side2: ";
+    cin >> side1 >> side2;
+    cout << endl;
+    
+
+    return 0;
+}
